a101cb7163303eb57f01fabd4a5f7ec441287a8b
YaemikoGit <102906443+YaemikoGit@users.noreply.github.com>
2025-05-14 23:05:35 +0800

    Added common folder for html/js
    home.js in backend is renamed to homeApi.js to avoid confusion

---
+++ frontend/common/common_js/include.js
@@ -0,0 +1,22 @@
+// Dynamically load shared HTML components
+window.addEventListener('DOMContentLoaded', () => {
+    const includes = {
+      header: '/common/header.html',
+      footer: '/common/footer.html',
+    };
+  
+    for (const [key, path] of Object.entries(includes)) {
+      const el = document.getElementById(`${key}-placeholder`);
+      if (el) {
+        fetch(path)
+          .then(res => res.text())
+          .then(html => {
+            el.innerHTML = html;
+          })
+          .catch(err => {
+            console.error(`Error loading ${key}:`, err);
+          });
+      }
+    }
+  });
+  
--- server.js
+++ server.js
@@ -3,14 +3,17 @@ const app = express();
 const port = 3000;
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'frontend/html')));
-app.use(express.static(path.join(__dirname, 'frontend')));
+
+app.use('/html', express.static(path.join(__dirname, 'frontend/html')));
+app.use('/js', express.static(path.join(__dirname, 'frontend/js')));
+app.use('/common', express.static(path.join(__dirname, 'frontend/common')));
+app.use('/static', express.static(path.join(__dirname, 'frontend/static')));
 // app.use(express.static(path.join(__dirname, 'frontend/js'))); // dt need this but leave for now
 
 app.use(express.json());
 
 // import route files
-const homeRoutes = require('./backend/routes/home');
+const homeRoutes = require('./backend/routes/homeApi');
 
 // using the routes
 app.use(homeRoutes);
@@ -19,6 +22,7 @@ app.use(homeRoutes);
 app.get('/', (req, res) => {
     res.redirect('/html/home.html');
 });
+
 
 // to run : npx nodemon server.js -> can try nodemon server.js if it works for yall
 app.listen(port, () => {
