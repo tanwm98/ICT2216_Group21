802fb0666e29a6834de20e94a904b0d20397968d
tanwm98 <2301777@sit.singaporetech.edu.sg>
2025-06-09 08:05:37 +0000

    ssl messing things up, will figure out later

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const jwt = require('jsonwebtoken');
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 
+
 function verifyToken(req, res, next) {
     const token = req.cookies.token;
 
